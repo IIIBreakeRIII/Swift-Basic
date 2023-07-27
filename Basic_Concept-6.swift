6f0e63fd53b583305b071f4feead9c2cc57dbe1d
IIIBreakeRIII <ryu990305@icloud.com>
2023-07-27 16:38:02 +0900

    Basic Concept 6

---
+++ Basic_Concept-6.swift
@@ -0,0 +1,30 @@
+// Concurrency - 비동기 / 동기 처리
+func fetchUserID(from server: String) async -> Int {
+    if server == "primary" {
+        return 97
+    }
+    return 501
+}
+
+func fetchUsername(from server: String) async -> String {
+    let userID = await fetchUserID(from: server)
+    if userID == 501 {
+        return "John Appleseed"
+    }
+    return "Guest"
+}
+
+func connectUser(to server: String) async {
+    async let userID = fetchUserID(from: server)
+    async let username = fetchUsername(from: server)
+    let greeting = await "Hello \(username), user ID \(userID)"
+    print(greeting)
+}
+
+Task {
+    await connectUser(to: "primary")
+}
+
+Task {
+    await connectUser(to: "apple")
+}
